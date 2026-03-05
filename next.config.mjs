a891017e6e5b7d8c0626d38ea4c258dae7688eb8
ramana <ramana.webronic@gmail.com>
2026-03-05 10:00:59 +0530

    Configure base path for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,8 @@ const nextConfig = {
     images: {
         unoptimized: true, // Disable default image optimization
     },
+    // Set basePath to repository name when running on GitHub Actions
+    basePath: process.env.GITHUB_ACTIONS && process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}` : '',
 };
 
 export default nextConfig;
